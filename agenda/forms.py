0ae45204b404d7360d11f83abd23ab2aef3e614e
Jose Madrigal <mac.yosef@gmail.com>
2015-01-08 14:51:46 -0600

    Agregar grupos y colores

--- agenda/forms.py
+++ agenda/forms.py
@@ -1,7 +1,15 @@
 from django import forms
-from agenda.models import Contactos
+from agenda.models import Contactos, Grupos, Colores
 
 
 class ContactoForm(forms.ModelForm):
-    class Meta:
-        model = Contactos
+	class Meta:
+	        model = Contactos
+
+class GroupForm(forms.ModelForm):
+	class Meta:
+		model = Grupos
+
+class ColorForm(forms.ModelForm):
+	class Meta:
+		model = Colores
--- agenda/urls.py
+++ agenda/urls.py
@@ -3,5 +3,12 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('agenda.views',
     url(r'^$', 'index', name="index"),
     url(r'^addContact/$', 'add_contact',  name='add_contact'),
-    url(r'^contacto/(?P<pk>.*)/$', 'contact_detail', name='contact_detail')
+    url(r'^contacto/(?P<pk>.*)/$', 'contact_detail', name='contact_detail'),
+    url(r'^addGroup/$','add_group', name='add_group'),
+    url(r'^Grupo/(?P<pk>.*)/$','group_detail',name='group_detail'),	
+    url(r'^ShowDelGrupo/$','delete',name='delete'),
+    url(r'^DelGrupo(?P<pk>.*)/$','delete_group',name='delete_group'),
+    url(r'^addColor/$','add_color',name='add_color'),
+    url(r'^Color(?P<pk>.*)/$','color_detail',name='color_detail'),
+
 )
